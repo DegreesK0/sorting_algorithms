a43b119f87572ace738d9d67fa40cebf6163254f
Richie911 <oohrichmond.k@gmail.com>
2024-01-22 10:57:46 -0800

    Add quick_sort file

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -12,25 +12,25 @@
  */
 listint_t *create_list(const int *array, size_t size)
 {
-    listint_t *list;
-    listint_t *node;
-    int *tmp;
+	listint_t *list;
+	listint_t *node;
+	int *tmp;
 
-    list = NULL;
-    while (size--)
-    {
-        node = malloc(sizeof(*node));
-        if (!node)
-            return (NULL);
-        tmp = (int *)&node->n;
-        *tmp = array[size];
-        node->next = list;
-        node->prev = NULL;
-        list = node;
-        if (list->next)
-            list->next->prev = list;
-    }
-    return (list);
+	list = NULL;
+	while (size--)
+	{
+		node = malloc(sizeof(*node));
+		if (!node)
+			return (NULL);
+		tmp = (int *)&node->n;
+		*tmp = array[size];
+		node->next = list;
+		node->prev = NULL;
+		list = node;
+		if (list->next)
+			list->next->prev = list;
+	}
+	return (list);
 }
 
 /**
@@ -46,7 +46,7 @@ void insertion_sort_list(listint_t **list)
 	int i, key, j;
 	listint_t *data = *list;
 	int *array;
-	
+
 	while (data != NULL)
 	{
 		count++;
---
+++ 3-quick_sort.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+/**
+ * swap - Swap function.
+ * @a: Pointer to the first integer.
+ * @b: Pointer to the second integer.
+ */
+void swap(int *a, int *b)
+{
+	int temp = *a;
+
+	*a = *b;
+	*b = temp;
+}
+
+/**
+ * lomuto_partition - Perform Lomuto partition on an array of integers.
+ * @array: Array of integers.
+ * @size: Size of the array.
+ * @low: Starting index of the partition.
+ * @high: Ending index of the partition.
+ *
+ * Return: Final partition index.
+ */
+int lomuto_partition(int *array, size_t size, int low, int high)
+{
+	int *pivot = array + high;
+	int i = low - 1;
+	int j;
+
+	for (j = low; j < high; j++)
+	{
+		if (array[j] < *pivot)
+		{
+			if (++i != j)
+			{
+				swap(array + i, array + j);
+				print_array(array, size);
+			}
+		}
+	}
+
+	if (array[++i] > *pivot)
+	{
+		swap(array + i, pivot);
+		print_array(array, size);
+	}
+
+	return i;
+}
+
+/**
+ * lomuto_sort - Implement quicksort.
+ * @array: Array of integers to sort.
+ * @size: Size of the array.
+ * @low: Starting index of the array.
+ * @high: Ending index of the array.
+ */
+void lomuto_sort(int *array, size_t size, int low, int high)
+{
+	int partition;
+
+	if (low < high)
+	{
+		partition = lomuto_partition(array, size, low, high);
+		lomuto_sort(array, size, low, partition - 1);
+		lomuto_sort(array, size, partition + 1, high);
+	}
+}
+
+/**
+ * quick_sort - Sort an array of integers
+ * @array: Array of integers.
+ * @size: Size of the array.
+ *
+ * Description: Prints the array after each swap.
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+
+	lomuto_sort(array, size, 0, size - 1);
+}
+
