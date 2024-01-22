b602f5f55bea046e9815ef931532979c06b82d20
DegreesK0 <kelvinbora@gmail.com>
2024-01-22 09:44:56 +0000

    Upload

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -27,30 +27,29 @@ void swap_ints(int *a, int *b)
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j, len = size;
-	size_t base = 1;
 
 	if (array == NULL || size < 2)
 		return;
 
 	for (i = 0; i <= len - 1; i++)
 	{
-		/* for (j = 0; j <= size - i - 1; j++) */
-		/* { */
-		/*	if (array[j] > array[j + 1]) */
-		/*	{ */
-		/*		swap_ints(&array[j], &array[j + 1]); */
-		/*		print_array(array, size); */
-		/*	} */
-		/* } */
-
-		for (j = len - 1; j >= base; j--)
+		for (j = 0; j <= len - i - 1; j++)
 		{
-			if (array[j - 1] > array[j])
+			if (array[j] > array[j + 1] && j != len - 1)
 			{
-				swap_ints(&array[j], &array[j - 1]);
+				swap_ints(&array[j], &array[j + 1]);
 				print_array(array, size);
 			}
 		}
-		base++;
+		len--;
+
+		/*for (j = size - 1; j >= 1; j--) */
+		/*{ */
+		/*	if (array[j - 1] > array[j]) */
+		/*	{ */
+		/*		swap_ints(&array[j], &array[j - 1]); */
+		/*		print_array(array, size); */
+		/*	} */
+		/*} */
 	}
 }
